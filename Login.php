de75f0dff9025b189a3b27bf94efeba956f3650b
Dev <munmunr509@gmail.com>
2021-09-06 11:27:56 +0600

    Login header designed

---
+++ Login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php  include('includes/header.php')  ?>
+
+<div class="no-bottom no-top" id="content">
+    <div id="top"></div>
+			
+		<section class="full-height relative no-top no-bottom vertical-center" data-bgimage="url(images/background/subheader-dark.jpg)">
+                <div class="overlay-gradient t50">
+					<div class="center-y relative">
+						<div class="container">
+							<div class="row align-items-center">
+								<div class="col-lg-5 text-light">
+                                <div class="spacer-10"></div>
+                                <h1>Login</h1>
+                                <p class="lead">Desc..</p>
+                            </div>
+						</div>
+					</div>
+				</div>
+		</section>		
+    </div>
+</div>
+<?php  include('includes/footer.php')  ?>
+
+<?php  include('includes/scripts.php')  ?>
+
+</body>
+
+</html>
