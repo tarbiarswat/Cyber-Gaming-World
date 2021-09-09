4f16132df5ff5027d64e8ad8e5b116b9647bb4ce
unknown <tarbiarswat9@gmail.com>
2021-09-09 21:41:37 +0600

    Admin Panel Navbar pages added

--- Login.php
+++ Login.php
@@ -6,22 +6,50 @@
 <div class="no-bottom no-top" id="content">
     <div id="top"></div>
 			
-		<section class="full-height relative no-top no-bottom vertical-center" data-bgimage="url(images/background/subheader-dark.jpg)">
-                <div class="overlay-gradient t50">
-					<div class="center-y relative">
-						<div class="container">
-							<div class="row align-items-center">
-								<div class="col-lg-5 text-light">
-                                <div class="spacer-10"></div>
-                                <h1>Login</h1>
-                                <p class="lead">Desc..</p>
-                            </div>
+		<section class="full-height relative no-top no-bottom vertical-center" data-bgimage="url(images/background/subheader-dark.jpg) top" data-stellar-background-ratio=".5">
+			<div class="overlay-gradient t50">
+				<div class="center-y relative">
+					<div class="container">
+						<div class="row align-items-center">
+							<div class="col-lg-5 text-light wow fadeInRight" data-wow-delay=".5s">
+							<div class="spacer-10"></div>
+							<h1>Login.</h1>
+							<p class="lead">Desc..</p>
+						</div>
+							
+							<div class="col-lg-4 offset-lg-2 wow fadeIn" data-wow-delay=".5s">
+								<div class="box-rounded padding40" data-bgcolor="#21273e">
+									<h3 class="mb10">Sign In</h3>
+									<p>Login using an existing account or create a new account <a href="register.html">here<span></span></a>.</p>
+									<form name="contactForm" id='contact_form' class="form-border" method="post" action='blank.php'>
+
+										<div class="field-set">
+											<input type='text' name='email' id='email' class="form-control" placeholder="username">
+										</div>
+										
+											<div class="field-set">
+											<input type='password' name='password' id='password' class="form-control" placeholder="password">
+										</div>
+										
+										<div class="field-set">
+											<input type='submit' id='send_message' value='Submit' class="btn btn-main btn-fullwidth color-2">
+										</div>
+										
+										<div class="clearfix"></div>
+										
+										<div class="spacer-single"></div>
+
+									</form>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
+			</div>
 		</section>		
+            
     </div>
-</div>
+</div>	
 <?php  include('includes/footer.php')  ?>
 
 <?php  include('includes/scripts.php')  ?>
---
+++ admin/includes/navbar.php
@@ -0,0 +1,108 @@
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+          <i class="fas fa-user-tie"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Hi, Admin <sup></sup></div>
+      </a>
+
+      <hr class="sidebar-divider my-0">
+
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+
+      <hr class="sidebar-divider">
+
+      <div class="sidebar-heading">
+        Core Interfaces
+      </div>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <i class="fas fa-tasks"></i>
+          <span>Tournaments</span>
+        </a>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Access Tournaments</h6>
+            <a class="collapse-item" href="#">Add Tournaments</a>
+            <a class="collapse-item" href="#">Tournaments List</a>
+          </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-tasks"></i>
+          <span>Game Updates</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Access Game Updates</h6>
+            <a class="collapse-item" href="#">Add Game Updates</a>
+            <a class="collapse-item" href="#">Game Updates List</a>
+          </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-swatchbook"></i>
+          <span>Games</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Access Games</h6>
+            <a class="collapse-item" href="#">Add New Game</a>
+            <a class="collapse-item" href="#">View Games</a>
+            
+          </div>
+        </div>
+      </li>
+
+      <hr class="sidebar-divider">
+
+      <div class="sidebar-heading">
+        Portals
+      </div>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Admin Resources</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">File Manager</h6>
+            <a class="collapse-item" href="#">Add A New File</a>
+            <a class="collapse-item" href="#">Uploaded Files</a>
+
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Other Pages:</h6>
+            <a class="collapse-item" href="#">Send Notifications</a>
+            <a class="collapse-item" href="#">Blank Page</a>
+          </div>
+        </div>
+      </li>
+
+
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fas fa-question-circle"></i>
+          <span>Feedbacks</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fas fa-network-wired"></i>
+          <span>Extras</span></a>
+      </li>
+
+      <hr class="sidebar-divider d-none d-md-block">
+
+    </ul>
