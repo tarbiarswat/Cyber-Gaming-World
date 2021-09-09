ca5be083d8ad68001356b0987e175c6dd83893ce
unknown <tarbiarswat9@gmail.com>
2021-09-09 23:40:08 +0600

    Admin panel multiple pages added with temp php

---
+++ members/memberslist.php
@@ -0,0 +1,33 @@
+<?php  
+include('includes/header.php'); 
+include('includes/navbar.php');
+include('includes/topbar.php');   
+?>
+
+<div id="content-wrapper" class="d-flex flex-column">
+    <div id="content">
+        <div class="container-fluid">
+
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">CGW Members</h1>
+            </div>
+
+        
+
+
+
+
+
+
+        </div>
+    </div>
+</div>
+  
+</body>
+
+</html>
+<?php  
+ 
+include('includes/scripts.php');  
+ 
+?>
