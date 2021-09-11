438452b5fbaa9498370122b83849cc9a31102a59
Munmunbb <munmunr509@gmail.com>
2021-09-11 22:15:27 +0600

    Add tournaments form designed

--- admin/add_tournaments.php
+++ admin/add_tournaments.php
@@ -4,12 +4,74 @@ include('includes/navbar.php');
 include('includes/topbar.php');   
 ?>
 
+
 <div id="content-wrapper" class="d-flex flex-column">
     <div id="content">
         <div class="container-fluid">
 
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 class="h3 mb-0 text-gray-800">Add Tournaments</h1>
+            </div>
+
+            <div class="card shadow mb-4">
+
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Tournament Details</h6>
+                </div>
+                
+
+                    <section class="section card mb-5">
+
+                        <div class="card-body">
+                            <div class="row">
+
+                                <div class="col-md-4 mb-4">
+
+                                    <div class="md-form md-outline">
+                                    <input type="text" id="f2" class="form-control">
+                                    <label for="f2" class="">Tournament Title</label>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+
+                                <div class="col-md-4 mb-4">
+
+                                    <div class="md-form md-outline">
+                                    <input type="text" id="f2" class="form-control">
+                                    <label for="f2" class="">Tournament Title</label>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="row text-left">
+
+                                <div class="col-md-6 mb-4">
+
+                                    <div class="md-form md-outline">
+                                    <textarea type="text" id="form101" class="md-textarea form-control" rows="3"></textarea>
+                                    <label for="form101">Basic</label>
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
+                    </section>
+
+
+
+                    
+
+                
             </div>
 
         
