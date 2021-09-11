29741ad3fc6ba6e980767f2e8c0d3d0a52fc66e7
Munmunbb <munmunr509@gmail.com>
2021-09-11 23:12:16 +0600

    Adding tournaments page from admin modified

--- admin/add_tournaments.php
+++ admin/add_tournaments.php
@@ -28,8 +28,8 @@ include('includes/topbar.php');
                                 <div class="col-md-4 mb-4">
 
                                     <div class="md-form md-outline">
-                                    <input type="text" id="f2" class="form-control">
                                     <label for="f2" class="">Tournament Title</label>
+                                    <input type="text" id="f2" class="form-control">
                                     </div>
 
                                 </div>
@@ -37,31 +37,100 @@ include('includes/topbar.php');
                             </div>
 
                             <div class="row">
-
-                                <div class="col-md-4 mb-4">
-
-                                    <div class="md-form md-outline">
-                                    <input type="text" id="f2" class="form-control">
-                                    <label for="f2" class="">Tournament Title</label>
-                                    </div>
-
+                                <div class="col-lg-4 col-md-6 mb-4">
+                                <label for="f2" class="">Game Name</label>
+                                    <fieldset class="form-check mb-4">
+                                    <input class="form-check-input" name="group1" type="radio" id="radio1" checked="checked">
+                                    <label class="form-check-label" for="radio1">Valorant &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                    <input class="form-check-input" name="group1" type="radio" id="radio2">
+                                    <label class="form-check-label" for="radio2">Call of Duty &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                    <input class="form-check-input" name="group1" type="radio" id="radio3">
+                                    <label class="form-check-label" for="radio3">GTA V Online &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                    </fieldset>
                                 </div>
 
+                                <div class="col-lg-4 col-md-6 mb-4">
+                                <label for="f2" class="">Tournament Type</label>
+                                    <fieldset class="form-check mb-4">
+                                    <input class="form-check-input" name="group2" type="radio" id="radio1" checked="checked">
+                                    <label class="form-check-label" for="radio1">Free &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                    <input class="form-check-input" name="group2" type="radio" id="radio2">
+                                    <label class="form-check-label" for="radio2">Paid &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                    
+                                    </fieldset>
+                                </div>
                             </div>
+
+                            <!--
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 mb-4">
+                                <label for="f2" class="">Tournament Type</label>
+                                    <fieldset class="form-check mb-4">
+                                    <input class="form-check-input" name="group1" type="radio" id="radio1" checked="checked">
+                                    <label class="form-check-label" for="radio1">Free &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                    <input class="form-check-input" name="group1" type="radio" id="radio2">
+                                    <label class="form-check-label" for="radio2">Paid &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                    
+                                    </fieldset>
+                                </div>
+                            </div>
+                            -->
+
 
                             <div class="row text-left">
 
                                 <div class="col-md-6 mb-4">
 
                                     <div class="md-form md-outline">
+                                    <label for="form101">Tournament Description</label>
                                     <textarea type="text" id="form101" class="md-textarea form-control" rows="3"></textarea>
-                                    <label for="form101">Basic</label>
                                     </div>
 
                                 </div>
 
 
                             </div>
+
+
+                            <div class="row">
+
+                                <div class="col-md-2 mb-4">
+
+                                    <div class="md-form md-outline">
+                                    <label for="f3" class="">Tournament Duration</label>
+                                    <input type="text" id="f3" class="form-control">
+                                    </div>
+
+                                </div>
+
+                                <div class="col-md-2 mb-4">
+
+                                    <div class="md-form md-outline">
+                                    <label for="f3" class="">Player Capacity</label>
+                                    <input type="text" id="f3" class="form-control">
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+                            <div class="row">
+
+                                <div class="col-md-2 mb-4">
+                                    <label for="f4" class="">Upload Featured Image</label>
+                                    <input type="file" class="btn btn-primary" data-toggle="modal">
+                                </div>
+
+                            </div>
+
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">
+                            Submit Data 
+                            </button>
+
+
+
+
 
                         </div>
 
