d3426977d7d40a495556b39ccb1c75677718fd4a
Munmunbb <munmunr509@gmail.com>
2021-09-18 14:42:36 +0600

    Organizers login and register frontend designed

---
+++ organizers/login.php
@@ -0,0 +1,82 @@
+<?php  
+session_start();
+include('includes/header.php');  
+?>
+
+<body class="bg-gradient-primary" style="background-image: url('img/subheader-dark.jpg');" >
+
+  <div class="container py-5">
+
+    <div class="row justify-content-center">
+
+      <div class="col-xl-6 col-lg-6 col-md-6">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <div class="row">
+              
+              <div class="col-lg-12">
+                <div class="p-5">
+                  <div class="text-center">
+                  <img class="img-profile" src="img/icons8-lock-40.png">
+                  
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+
+                    <?php
+                      if(isset($_SESSION['status']) && $_SESSION['status'] !='' )
+                      {
+                        echo '<h4 class= "bg-danger text-white"> '.$_SESSION['status'].' </h4>';
+                        unset($_SESSION['status']);
+                      }
+                    ?>
+                    
+ 
+                  </div>
+                  <form class="user" action="class.php" method="POST">
+                    <div class="form-group">
+                      <input type="email" name="email_id" class="form-control form-control-user"  placeholder="Enter Email Address...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name="password" class="form-control form-control-user"  placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+                    <button type="submit" name= "login_btn"  class="btn btn-primary btn-user btn-block">  Login</button>
+                
+                
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="#">Forgot Password?</a>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</body>
+
+
+
+
+
+
+
+
+
+<?php  
+include('includes/scripts.php');  
+
+?>
---
+++ organizers/o-register.php
@@ -0,0 +1,83 @@
+<?php  
+session_start();
+include('includes/header.php');  
+?>
+
+<body class="bg-gradient-primary" style="background-image: url('img/subheader-dark.jpg');" >
+
+  <div class="container py-5">
+
+  <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block" style="background: url('img/o-register-bg.jpg');background-position: center;background-size: cover;" ></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Register as Organizer!</h1>
+                            </div>
+                            <form class="user">
+
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-4 mb-sm-0">
+                                        <input type="text" name="username" class="form-control form-control-user" id="username" placeholder="username">
+                                    </div>
+                                    <div class="col-sm-6 mb-4 mb-sm-0">
+                                        <input type="text" name="fullName" class="form-control form-control-user" id="fullName" placeholder="Enter Full Name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email_id" class="form-control form-control-user" id="email_id" placeholder="Email Address">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" name="password" class="form-control form-control-user" id="password" placeholder="password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" name="cpassword" class="form-control form-control-user" id="cpassword" placeholder="Repeat Password">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                  <div class="col-sm-4 mb-sm-0">
+                                    <label for="">Upload Profile Photo</label>
+                                  <input type="file" class="btn btn-primary btn-user "></input>
+                                  </div>
+                                </div>
+
+                                <button type="submit" name="organizers_register" class="btn btn-primary btn-user btn-block">Register Account</button>
+                                <hr>
+                                <h1 class="h4 text-gray-900 mb-4" style="text-align:center;" >OR</h1>
+                                <button class="btn btn-primary btn-user btn-block">Log in</button>
+                
+                            </form>
+                            <hr>
+
+                            <div class="text-center">
+                                <a class="small" href="">Forgot Password?</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+  </div>
+
+</body>
+
+
+
+
+
+
+
+
+
+<?php  
+include('includes/scripts.php');  
+
+?>
